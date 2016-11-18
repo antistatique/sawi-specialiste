cfa2096c385032a1ea7333dcd1764b6abfb65180
Kevin Wenger <kevin@antistatique.net>
2016-11-18 18:13:42 +0100

    Menu

--- assets/js/menu_toggle.js
+++ assets/js/menu_toggle.js
@@ -1,8 +1,17 @@
 import $ from 'jquery';
 
 export function menu_toggle () {
-  $('.menu-toggle').click(function(e){
+  $(document).on('click', '.menu-toggle', function(e){
     e.preventDefault();
-    $(this).toggleClass('open');
+
+    const $this = $(this);
+    const $body = $(document.body);
+    const $menu = $('.header-menu');
+    const $overlay = $('.body-overlay');
+
+    $this.toggleClass('active');
+    $body.toggleClass('menu-push-toright');
+    $menu.toggleClass('menu-open');
+    $overlay.toggleClass('visible');
   });
 }
