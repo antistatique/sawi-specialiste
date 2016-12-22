e4371e93c47d5342c802906e2cbe386376ef2895
Kevin Wenger <kevin@antistatique.net>
2016-12-22 16:44:12 +0100

    Fix timeline loading event

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,21 +1,19 @@
-/* globals TL */
+/* globals TL drupalSettings */
 
 export function timeline () {
-  console.log(drupalSettings.sawi_site.timeline);
-
   const options = {
     debug: true,
     optimal_tick_width: 300,
-    base_class: 'sawi-test',
-    language: 'fr',
+    base_class: 'sawi-timeline',
+    language: typeof drupalSettings != 'undefined' ? drupalSettings.language : 'fr',
     timenav_height: 250,
     marker_height_min: 40,
     marker_padding: 10
   };
 
   if (typeof TL !== 'undefined') {
-    if (typeof drupalSettings !== 'undefined') {
-      const timeline = new TL.Timeline('timeline-embed', drupalSettings.sawi_site.timeline, options);
+    if (typeof drupalSettings !== 'undefined' && typeof drupalSettings.sawi_site.timeline_url !== 'undefined') {
+      const timeline = new TL.Timeline('timeline-embed', drupalSettings.sawi_site.timeline_url, options);
     }
     else {
       // Parameter used to avoid caching
