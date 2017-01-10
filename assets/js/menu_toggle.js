9e93b48a96fe8098a47ce7c2c8c63300ac3491e9
wengerk <wenger.kev@gmail.com>
2017-01-10 16:11:33 +0100

    Improved menu toggle with esc and skip link opening

--- assets/js/menu_toggle.js
+++ assets/js/menu_toggle.js
@@ -1,18 +1,36 @@
 import $ from 'jquery';
 
 export function menu_toggle () {
-  $(document).on('click', '.menu-toggle', function(e){
-    e.preventDefault();
-
+  $(document).on('click', '.menu-toggle, .menu-access .menu', function(e){
     const $this = $(this);
-    const $hamburger = $this.find('.hamburger');
-    const $body = $(document.body);
-    const $menu = $('.header-menu');
-    const $overlay = $('.body-overlay');
+
+    menu_toggle();
+
+    // Open the menu when using skip-link
+    if ($this.attr('href') != '#main-navigation') {
+      e.preventDefault();
+    }
+  });
+
+  // Close the menu when ESC is pressed
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      const $body = $(document.body);
+      if ($body.hasClass('menu-push-toright')) {
+        menu_toggle();
+      }
+    }
+  });
+
+  function menu_toggle() {
+    const $hamburger = $('header .hamburger');
+    const $body      = $(document.body);
+    const $menu      = $('.header-menu');
+    const $overlay   = $('.body-overlay');
 
     $hamburger.toggleClass('is-active');
     $body.toggleClass('menu-push-toright');
     $menu.toggleClass('menu-open');
     $overlay.toggleClass('visible');
-  });
+  }
 }
