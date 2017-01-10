fd1779e6877622be0547a0a94c06dab2011f7ca5
wengerk <wenger.kev@gmail.com>
2017-01-10 16:11:45 +0100

    Improved search box toggle with esc closing

--- assets/js/search_box.js
+++ assets/js/search_box.js
@@ -2,23 +2,40 @@ import $ from 'jquery';
 
 export function search_box () {
   $('.search-toggle').click(function(e){
+    e.preventDefault();
+    const $html = $('html');
+
     $(this).toggleClass('open');
     $('.search-box-modal').toggleClass('visible');
 
     if($(this).hasClass('open')){
-      $('html').addClass('modal-open');
+      $html.addClass('modal-open');
       $('.search-box-modal .form-control').focus();
     }else {
-      $('html').removeClass('modal-open');
+      $html.removeClass('modal-open');
     }
-
-    e.preventDefault();
   });
 
   $('.search-box-modal .close').click(function(e){
+    e.preventDefault();
+    search_box_close();
+  });
+
+  // Close the menu when ESC is pressed
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      const $search_toggle = $('.search-toggle');
+
+      if ($search_toggle.hasClass('open')) {
+        search_box_close();
+      }
+    }
+  });
+
+  function search_box_close() {
+    const $html = $('html');
     $('.search-toggle').removeClass('open');
     $('.search-box-modal').removeClass('visible');
-    $('html').removeClass('modal-open');
-    e.preventDefault();
-  });
+    $html.removeClass('modal-open');
+  }
 }
