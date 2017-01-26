21f97005a2156885e92ba876d7df5ee8cba2eb98
wengerk <wenger.kev@gmail.com>
2017-01-26 14:51:58 +0100

    Improved same-height & lists

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ import { menu_toggle } from './menu_toggle.js';
 import { timeline } from './timeline.js';
 import { search_box } from './search_box.js';
 import { googlemap } from './googlemap.js';
+import { same_height } from './same_height.js';
 
 (function(){
   menu_toggle();
@@ -10,5 +11,7 @@ import { googlemap } from './googlemap.js';
   window.addEventListener('load', function() {
     timeline();
     googlemap();
+    same_height();
+    
   });
 }());
