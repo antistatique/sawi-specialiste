1c2f6924628f1ca5ef55901a111f858c9ebc4557
wengerk <wenger.kev@gmail.com>
2017-02-22 15:51:22 +0100

    Added Gallery

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,15 @@ import { menu_toggle } from './menu_toggle.js';
 import { timeline } from './timeline.js';
 import { search_box } from './search_box.js';
 import { googlemap } from './googlemap.js';
+import { gallery } from './gallery.js';
 
 (function(){
   menu_toggle();
   search_box();
+  gallery();
 
   window.addEventListener('load', function() {
     timeline();
     googlemap();
-    
   });
 }());
