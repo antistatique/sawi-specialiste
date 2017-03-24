859df70d1c5e4ede4ad4bf903893626ae871dc59
wengerk <wenger.kev@gmail.com>
2017-03-24 10:54:40 +0100

    Added RangeSlider

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,13 @@ import { timeline } from './timeline.js';
 import { search_box } from './search_box.js';
 import { googlemap } from './googlemap.js';
 import { gallery } from './gallery.js';
+import { rangeslider } from './rangeslider.js';
 
 (function(){
   menu_toggle();
   search_box();
   gallery();
+  rangeslider();
 
   window.addEventListener('load', function() {
     timeline();
---
+++ assets/js/rangeslider.js
@@ -0,0 +1,68 @@
+/*global noUiSlider */
+
+export function rangeslider () {
+
+  // /////////////////////////////////////////////////////////////////////////////////////
+  // vars
+  const $rangeSliders = document.querySelectorAll('.input-rangeslider-range');
+
+  // /////////////////////////////////////////////////////////////////////////////////////
+  // binding
+  if ($rangeSliders != null) {
+    for (let $rangeSlider of $rangeSliders) {
+
+      console.log($rangeSlider.getAttribute('data-min'));
+      console.log($rangeSlider.getAttribute('data-max'));
+      noUiSlider.create($rangeSlider, {
+        start: parseInt($rangeSlider.getAttribute('data-start')),
+        step: 1,
+        // Disable animation on value-setting,
+        // so the sliders respond immediately.
+        animate: false,
+        range: {
+          min: parseInt($rangeSlider.getAttribute('data-min')),
+          max: parseInt($rangeSlider.getAttribute('data-max'))
+        }
+      });
+
+      const $rangeSliderInput = document.querySelector('input[name="'+$rangeSlider.getAttribute('data-input')+'"]');
+      $rangeSlider.noUiSlider.on('update', function(values, handle){
+        const $rangeSliderInput = document.querySelector('input[name="'+this.target.getAttribute('data-input')+'"]');
+        $rangeSliderInput.value = Math.round(values[handle]);
+      });
+
+      $rangeSlider.noUiSlider.on('slide', function(values, handle){
+        const $linkedSlider = document.querySelector('.'+this.target.getAttribute('data-linked-slider'));
+        if ($linkedSlider != null && typeof $linkedSlider.noUiSlider !== 'undefined') {
+          crossUpdate(values[handle], $linkedSlider);
+        }
+      });
+
+      $rangeSliderInput.addEventListener('blur', function () {
+        const $rangeSlider = document.querySelector('[data-input="'+this.getAttribute('name')+'"]');
+
+        if (this.value > parseInt($rangeSlider.getAttribute('data-max'))) {
+          this.value = $rangeSlider.getAttribute('data-max');
+        }
+
+        if (this.value < parseInt($rangeSlider.getAttribute('data-min'))) {
+          this.value = $rangeSlider.getAttribute('data-min');
+        }
+
+        $rangeSlider.noUiSlider.set(this.value);
+        const $linkedSlider = document.querySelector('.'+$rangeSlider.getAttribute('data-linked-slider'));
+        if ($linkedSlider != null && typeof $linkedSlider.noUiSlider !== 'undefined') {
+          crossUpdate(this.value, $linkedSlider);
+        }
+      });
+    }
+  }
+
+  function crossUpdate ( value, slider ) {
+    const total = slider.getAttribute('data-max') - value;
+    slider.noUiSlider.set(total);
+    const $rangeSliderInput = document.querySelector('input[name="'+slider.getAttribute('data-input')+'"]');
+    $rangeSliderInput.value = total;
+  }
+
+}
